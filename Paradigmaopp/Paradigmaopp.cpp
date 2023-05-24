e7ccbca1683398b293b98c6d009963ad0adb5caa
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-05-24 13:08:04 +0700

    membuat class pelajar sama dengan public orang

--- Paradigmaopp/Paradigmaopp.cpp
+++ Paradigmaopp/Paradigmaopp.cpp
@@ -19,28 +19,4 @@ public:
 	}
 
 };
-
 class pelajar : public orang {
-public:
-	string sekolah;
-
-	pelajar(string pNama, string pSekolah) :
-		orang(pNama),
-		sekolah(pSekolah){
-		cout << "pelajar dibuat\n" << endl;
-	}
-	~pelajar() {
-		cout << "pelajar dihapus\n" << endl;
-	}
-	string perkenalan() {
-		return "Hello, nama saya " + nama + " Dari sekolah" + sekolah + "\n\n";
-	}
-};
-
-int main() {
-	pelajar siswa("andi laksono", " SMAN 1 Bantul");
-	cout << siswa.perkenalan();
-	cout << "Hasil = " << siswa.jumlah(10, 90) << endl;
-
-	return 0;
-}
