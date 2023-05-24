7a20cc7afe0a133c9f4d85c439b5e26fafb258b8
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-05-24 14:36:02 +0700

    membuat class jantung dengan public

---
+++ mainJM/jantung.h
@@ -0,0 +1,16 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class Jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif // !JANTUNG_H
+
